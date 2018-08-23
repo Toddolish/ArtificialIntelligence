7fa1741d515f842acf79c704e2e04b8f0a0a4605
Toddolish <42237447+Toddolish@users.noreply.github.com>
2018-08-23 11:46:52 +1000

    NEW STUFF
    new ai and assets

---
+++ Assets/SteeringBehaviours/Scripts/AI/AIAgent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SteeringBehaviours
+{
+    public class AIAgent : MonoBehaviour
+    {
+        public NavMeshAgent agent;
+        public Transform target;
+
+        void Update()
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
