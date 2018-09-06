e46c6a67a7d852fb181742348119987ea3f726a9
Toddolish <42237447+Toddolish@users.noreply.github.com>
2018-09-06 12:02:40 +1000

    update
    learning about inheritance

---
+++ Assets/SteeringBehaviours/Scripts/AI/Flee.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SteeringBehaviours
+{
+    public class Flee : SteeringBehaviour
+    {
+        [Header("Fleeing")]
+        public Transform target;
+
+        public override Vector3 GetForce()
+        {
+            return base.GetForce();
+        }
+
+    }
+}
---
+++ Assets/SteeringBehaviours/Scripts/AI/Seek.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SteeringBehaviours
+{
+    public class Seek : SteeringBehaviour
+    {
+        [Header("Seeking")]
+        public Transform target;
+        public float stoppingDistance;
+
+        public override Vector3 GetForce()
+        {
+            return base.GetForce();
+        }
+    }
+}
